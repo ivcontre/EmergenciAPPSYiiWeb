06e7f95b2d1e02a72c0771d4d271214189945c26
Cristobal <Cristobal@Cristbal>
2014-10-16 16:49:37 -0300

    Se agrega servicio web gcm

--- protected/views/ws/registro_cliente_gcm.php
+++ protected/views/ws/registro_cliente_gcm.php
@@ -4,14 +4,14 @@ if(isset($_POST['id_contacto'])){
        $numero_telefono = $_POST['id_contacto']; 
 }
 
-if(isset($_POST['regedit'])){
-       $regedit = $_POST['regedit']; 
+if(isset($_POST['regid'])){
+       $regid = $_POST['regid']; 
 }
 
 $link = mysql_connect("localhost","rhormaza","czeSCfCQ");
 mysql_select_db("rhormaza",$link);
 
-$consulta = "UPDATE usuario SET regedit = '".$regedit."' WHERE numero_telefono = '".$numero_telefono."';";
+$consulta = "UPDATE usuario SET regid = '".$regid."' WHERE numero_telefono = '".$numero_telefono."';";
 
 mysql_query($consulta,$link) or die("false");
 
