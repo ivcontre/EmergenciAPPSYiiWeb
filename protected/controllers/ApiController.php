2949f896eb88b7ccc313871145d3da88c8b86256
Cristobal <Cristobal@Cristbal>
2014-11-12 01:18:16 -0300

    SE AGREGA: 	- SW en Apicontroller

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -473,6 +473,101 @@ class ApiController extends Controller {
         
         
     }
+    public function actionActualizaNumeroCarabinero(){
+        if (isset($_REQUEST['id_usuario']) && isset($_REQUEST['numero_carabinero'])) {
+            $id = $_REQUEST['id_usuario'];
+            $numero_carabinero = $_REQUEST['numero_carabinero'];
+        }
+        $usuario = Usuario::model()->findByPk($id);
+        
+        
+        
+        if($usuario != null){
+            $configuracion = $usuario->configuracion;
+            
+            if($configuracion != null){
+               $configuracion->numero_carabinero = $numero_carabinero; 
+            }else{
+                echo "modelo configuracion vacio";
+            }
+            
+            
+        }else{
+            echo "modelo usuario vacio";
+        }
+        
+        if($configuracion->save()){
+            echo "true";
+        }else{
+            echo "false";
+        }
+        
+        
+    }
+    
+    public function actionActualizaNumeroBombero(){
+        if (isset($_REQUEST['id_usuario']) && isset($_REQUEST['numero_bombero'])) {
+            $id = $_REQUEST['id_usuario'];
+            $numero_bombero = $_REQUEST['numero_bombero'];
+        }
+        $usuario = Usuario::model()->findByPk($id);
+        
+        
+        
+        if($usuario != null){
+            $configuracion = $usuario->configuracion;
+            
+            if($configuracion != null){
+               $configuracion->numero_bombero = $numero_bombero; 
+            }else{
+                echo "modelo configuracion vacio";
+            }
+            
+            
+        }else{
+            echo "modelo usuario vacio";
+        }
+        
+        if($configuracion->save()){
+            echo "true";
+        }else{
+            echo "false";
+        }
+        
+        
+    }
+    
+    public function actionActualizaNumeroCentroMedico(){
+        if (isset($_REQUEST['id_usuario']) && isset($_REQUEST['numero_centro_medico'])) {
+            $id = $_REQUEST['id_usuario'];
+            $numero_centro_medico = $_REQUEST['numero_centro_medico'];
+        }
+        $usuario = Usuario::model()->findByPk($id);
+        
+        
+        
+        if($usuario != null){
+            $configuracion = $usuario->configuracion;
+            
+            if($configuracion != null){
+               $configuracion->numero_centro_medico = $numero_centro_medico; 
+            }else{
+                echo "modelo configuracion vacio";
+            }
+            
+            
+        }else{
+            echo "modelo usuario vacio";
+        }
+        
+        if($configuracion->save()){
+            echo "true";
+        }else{
+            echo "false";
+        }
+        
+        
+    }
 
 }
 
